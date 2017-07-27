56ce2001507b5d18b92d402abfea940f2a0bce86
Leslie Lee <leslie@krossover.com>
2017-07-27 14:28:12 -0400

    use of hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,42 @@
-module.exports = function(env) {
-  return require(`./webpack.${env}.js`)
-}
+// module.exports = function(env) {
+//   return require(`./webpack.${env}.js`)
+// }
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase: './dist',
+        port: 8888,
+        host: 'localhost',
+        hot: true
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new ManifestPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hot module replacement'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output:{
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
